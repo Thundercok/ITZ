0c57624fff18b2c4a801a0af58e30b9a3d3b5b19
iamchouty <quocchau4729@gmail.com>
2024-04-19 23:38:23 +0700

    tutorial

--- ITZone/src/pages/Main/index.jsx
+++ ITZone/src/pages/Main/index.jsx
@@ -4,15 +4,15 @@ const Main = () => {
   return (
     <>
       <Header />
-      <div className="flex items-center w-full h-auto pt-40 roboto_slab ring-1">
+      <div className="flex items-center w-full h-auto pt-40 roboto_slab">
         <div className="left w-7/12 h-2/3">
-          <div className="image-holder w-full ring-1">
+          <div className="image-holder w-full">
             <img src="" alt="" />
-            <h1>Hello image</h1>
+            <h1>Hello</h1>
           </div>
         </div>
 
-        <div className="right p-10 pr-24 ring-1 ">
+        <div className="right p-10 pr-24">
           <h1 className="flex justify-end w-full pb-6 text-5xl font-bold">
             IT-Zone xin chào!
           </h1>
