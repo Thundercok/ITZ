4c63c1b85678a0b79bc9b19cee84cb13287bec4c
Hài Dón <148691850+Thundercok@users.noreply.github.com>
2024-05-01 23:29:59 +0700

    Initial commit

--- ITZone/src/components/User_Header/index.jsx
+++ ITZone/src/components/User_Header/index.jsx
@@ -3,10 +3,26 @@ import { Link } from "react-router-dom";
 
 const User_Header = () => {
   return (
-    <header>
-      {/* Header content goes here */}
-    </header>
+    <>
+      <div className="flex justify-center w-screen h-auto p-4 quick_sand">
+        <ul className="header-default flex justify-around w-4/5 text-2xl">
+          <Link className="header-font" to="/">
+            Trang chủ
+          </Link>
+          <Link className="header-font" to="/booking">
+            Đặt lịch
+          </Link>
+          <Link className="header-font" to="/feedback">
+            Phản hồi
+          </Link>
+          <Link className="header-font" to="/login">
+            Thành viên
+          </Link>
+        </ul>
+      </div>
+    </>
   );
-}
+};
+
 
 export default User_Header;
